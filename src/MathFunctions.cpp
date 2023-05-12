48a7b70e2c121010c70f4a1a7358767e63fda8bc
NiceGuy <bogdan.game.development@gmail.com>
2023-05-12 12:36:24 +0300

    deleted src for submodule

--- src/MathFunctions.cpp
+++
@@ -1,8 +0,0 @@
-#include "MathFunctions.hpp"
-#include <cfloat>
-#include <algorithm>
-
-bool MathFunctions::areEqualRel(float a, float b)
-{
-	return (fabs(a - b) <= FLT_EPSILON * std::max(fabs(a), fabs(b)));
-}
